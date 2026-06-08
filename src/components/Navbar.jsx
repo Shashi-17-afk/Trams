4c4c665a30a28508942d96a9fdcfc053763e3be7
Shashi Shanthan <shashishanthan2706@gmail.com>
2026-06-08 15:55:56 +0530

    Created Navbar , Content and Footer in components Folder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <img src="/Logo.png" alt="Trams logo" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
